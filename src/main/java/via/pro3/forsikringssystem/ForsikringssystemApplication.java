3446c23faa84d5f648b88ec215e40bb13c694826
Sakaadar <79919443+Sakaadar@users.noreply.github.com>
2025-01-27 16:20:48 +0100

    Start

---
+++ src/main/java/via/pro3/forsikringssystem/ForsikringssystemApplication.java
@@ -0,0 +1,14 @@
+package via.pro3.forsikringssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class ForsikringssystemApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(ForsikringssystemApplication.class, args);
+  }
+
+}
---
+++ src/test/java/via/pro3/forsikringssystem/ForsikringssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package via.pro3.forsikringssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class ForsikringssystemApplicationTests
+{
+
+  @Test void contextLoads()
+  {
+  }
+
+}
